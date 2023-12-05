0bcd1d301cc6dbdffde1f50d1bc05fb94ad04dae
alexander-karmanov <lektor_md@mail.ru>
2023-12-05 23:36:58 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/grabber/utils/HabrCareerDateTimeParser.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
-    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     @Override
     public LocalDateTime parse(String parse) {
--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -1,19 +1,19 @@
 package ru.job4j.grabber.utils;
 
+import grabber.utils.DateTimeParser;
 import grabber.utils.HabrCareerDateTimeParser;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HabrCareerDateTimeParserTest {
     @Test
     public void whenParseTextToDate() {
-        String textDate = "2023-09-20T12:36:39.359";
-        String textOtherDate = "2023-09-15T12:36:39.359";
-        LocalDateTime time = new HabrCareerDateTimeParser().parse(textDate);
-        assertThat(time).isInstanceOf(LocalDateTime.class);
-        assertThat(time).isEqualTo(LocalDateTime.parse(textDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        assertThat(time).isAfter(LocalDateTime.parse(textOtherDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        DateTimeParser parser = new HabrCareerDateTimeParser();
+        String textDate = "2023-11-13T11:32:03+03:00";
+        LocalDateTime ex = LocalDateTime.of(2023, Month.NOVEMBER, 13, 11, 32, 03);
+        assertThat(ex).isEqualTo(parser.parse(textDate));
     }
 }
